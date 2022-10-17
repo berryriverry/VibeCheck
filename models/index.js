4eef88dc58a54b9d2e97f744cd0a21d47e4dc8ff
Your Name <joelebell@gmail.com>
2022-10-17 19:35:04 -0400

    Second changes

--- models/index.js
+++ models/index.js
@@ -1,67 +1,28 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express')
+import fs        from "fs"
+import path      from "path"
+import Sequelize from "sequelize"
 
-const scopes = [
-    'streaming',
-];
-
-var spotifyApi = new SpotifyWebApi({
-    clientId: '',
-    clientSecret: '',
-    redirectUri: 'http://localhost:8888/callback'
-}),
-
-const app = express();
-
-app.get('/login', (req, res) => {
-    res.redirect(spotifyApi.createAuthorizeURL(scopes));
-});
-
-app.get('/callback', (req, res) => {
-    const error = req.query.error;
-    const code = req.query.code;
-    const state = req.query.code;
-
-    if (error) {
-        console.error('Callback Error:', error);
-        res.send(`Callback Error: ${error}`);
-        return;
-    }
-
-    spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      const access_token = data.body['access_token'];
-      const refresh_token = data.body['refresh_token'];
-      const expires_in = data.body['expires_in'];
-
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
-
-      console.log('access_token'), access_token;
-      console.log('refresh_token'), refresh_token;
-
-      console.log(
-        `Sucessfully retreived access token. Expires in ${expires_in} s.`
-      );
-      res.send('Success! You can now close the window.');
-
-      setInterval(async () => {
-        const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body['access_token'];
-
-        console.log('The access token has been refreshed!');
-        console.log('access_token:', access_token);
-        
-        spotifyApi.setAccessToken(access_token);
-      }, expires_in / 5 * 1000);
-    
-    expexted(error => {
-        console.error('Error getting Tokens:', error);
-        res.send(`Error getting Tokens: ${error}`);
-      });
-  
-
-  app.listen(8888, () =>
-  console.log('HTTP Server up. Now go to http://localhost:8888/login in your browser.')
-  );
+let db_url    = process.env.DATABASE
+let sequelize = new Sequelize(db_url)
+let db        = {}
+ 
+fs
+	.readdirSync(__dirname)
+	.filter(function(file) {
+		return (file.indexOf(".") !== 0) && (file !== "index.js")
+	})
+	.forEach(function(file) {
+		var model = sequelize.import(path.join(__dirname, file))
+		db[model.name] = model
+	});
+ 
+Object.keys(db).forEach(function(modelName) {
+	if (db[model.name].associate) {
+    db[model.name].associate(db)
+  }
+})
+ 
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+ 
+export default db
