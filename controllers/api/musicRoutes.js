c50e022d9abc981f8fb7b505fef86c716fd5370e
Your Name <joelebell@gmail.com>
2022-10-20 13:52:18 -0400

    Music Route edits

--- controllers/api/musicRoutes.js
+++ controllers/api/musicRoutes.js
@@ -1,67 +1,88 @@
-var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express');
+const router = require('express').Router();
+const { Playlist, Song } = require('../models');
 
-const scopes = [
-    'streaming',
-];
+router.get('/', async (req, res) => {
+  try {
+    const dbPlaylistData = await Playlist.findAll({
+      include: [
+        {
+          model: Song,
+          attributes: ['filename', 'description'],
+        },
+      ],
+    });
 
-var spotifyApi = new SpotifyWebApi({
-    clientId: '',
-    clientSecret: '',
-    redirectUri: 'http://localhost:8888/callback'
-}),
+    const Playlist = dbPlaylistData.map((playlist) =>
+      playlist.get({ plain: true })
+    );
 
-const app = express();
-
-app.get('/login', (req, res) => {
-    res.redirect(spotifyApi.createAuthorizeURL(scopes));
+    res.render('homepage', {
+      playlist,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-app.get('/callback', (req, res) => {
-    const error = req.query.error;
-    const code = req.query.code;
-    const state = req.query.code;
+router.get('/playlist/:id', async (req, res) => {
 
-    if (error) {
-        console.error('Callback Error:', error);
-        res.send(`Callback Error: ${error}`);
-        return;
-    }
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
 
-    spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-      const access_token = data.body['access_token'];
-      const refresh_token = data.body['refresh_token'];
-      const expires_in = data.body['expires_in'];
-
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
-
-      console.log('access_token'), access_token;
-      console.log('refresh_token'), refresh_token;
-
-      console.log(
-        `Sucessfully retreived access token. Expires in ${expires_in} s.`
-      );
-      res.send('Success! You can now close the window.');
-
-      setInterval(async () => {
-        const data = await spotifyApi.refreshAccessToken();
-        const access_token = data.body['access_token'];
-
-        console.log('The access token has been refreshed!');
-        console.log('access_token:', access_token);
-        
-        spotifyApi.setAccessToken(access_token);
-      }, expires_in / 5 * 1000);
-    
-    expexted(error => {
-        console.error('Error getting Tokens:', error);
-        res.send(`Error getting Tokens: ${error}`);
+    try {
+      const dbPlaylistData = await Playlist.findByPk(req.params.id, {
+        include: [
+          {
+            model: Song,
+            attributes: [
+              'id',
+              'title',
+              'artist',
+              'release_date',
+              'filename',
+              'description',
+            ],
+          },
+        ],
       });
-  
+      const Playlist = dbPlaylistData.get({ plain: true });
+      res.render('playlist', { playlist, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
 
-  app.listen(8888, () =>
-  console.log('HTTP Server up. Now go to http://localhost:8888/login in your browser.')
-  );
+router.get('/song/:id', async (req, res) => {
+
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+
+    try {
+      const dbSongData = await Song.findByPk(req.params.id);
+
+      const song = dbSongData.get({ plain: true });
+
+      res.render('song', { song, loggedIn: req.session.loggedIn });
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  }
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
