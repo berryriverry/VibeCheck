47817888bd253d604703c59db08de3f33fa181d2
Graham Johnson <107967591+graham1996@users.noreply.github.com>
2022-10-24 14:58:37 -0400

    pushing to main for heroku deployment

--- public/js/login.js
+++ public/js/login.js
@@ -1,32 +1,3 @@
-// const loginFormHandler = async (event) => {
-//     event.preventDefault();
-  
-//     const email = document.querySelector('#email-login').value.trim();
-//     const password = document.querySelector('#password-login').value.trim();
-  
-//     if (email && password) {
-//       const response = await fetch('/api/users/login', {
-//         method: 'POST',
-//         body: JSON.stringify({ email, password }),
-//         headers: { 'Content-Type': 'application/json' },
-//       });
-  
-//       if (response.ok) {
-//         document.location.replace('/');
-//       } else {
-//         alert('Failed to log in');
-//       }
-//     }
-//   };
-  
-//   document
-//     .querySelector('.login-form')
-//     .addEventListener('submit', loginFormHandler);
-
-
-
-
-
 var Login = function() {
   this.client_id = process.env.CLIENT_ID;
   this.access_token = process.env.CLIENT_SECRET;
